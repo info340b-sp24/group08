e1392debda3d96e23f3428dbf3916922936ced71
yuan_ <user@example.com>
2024-05-27 23:13:30 -0700

    Integrate new React Library

--- src/Listings.js
+++ src/Listings.js
@@ -1,167 +1,204 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { Card, Carousel, Form, Button, Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  dataImg from './img/Data.jpg';
+import UIUX from './img/UIUX.jpg';
+import software from './img/Software.jpg';
+
 
 const Listings = () => {
-    const initialJobs = [
-        {
-            id: 1,
-            title: 'Jr. Software Development Engineer',
-            company: 'Amazon',
-            location: 'Seattle, WA',
-            industry: 'Technology',
-            salaryRange: '60,000-70,000',
-            highlights: [
-                'Part-time work during the school year (16 hours/week)',
-                'Full-time work during the summer (40 hours/week)',
-                'Effective performance management and integrated opportunities for growth'
-            ]
-        },
-        {
-            id: 2,
-            title: 'Data Analytics Intern',
-            company: 'DHL Express',
-            location: 'Dallas, TX',
-            industry: 'Logistics',
-            salaryRange: '30,000-40,000',
-            highlights: [
-                'Strong communication skills (written and verbal)',
-                'Strong attention to detail and organizational skills',
-                'Knowledge of business intelligence tools such as Power BI, Tableau'
-            ]
-        },
-        {
-            id: 3,
-            title: 'Tax Data Analyst',
-            company: 'Salesforce',
-            location: 'Seattle, WA',
-            industry: 'Technology',
-            salaryRange: '70,000-80,000',
-            highlights: [
-                'Experience telling stories with data and familiarity with Tableau visualization tool',
-                'Experience applying your analytics skills to projects which have had impact on business strategy',
-                'Interest in unlocking new opportunities for growth by discovering insights, automating processes'
-            ]
-        },
-        {
-            id: 4,
-            title: 'Data Analyst, Supply Chain',
-            company: 'Tesla',
-            location: 'Fremont, CA',
-            industry: 'Automotive',
-            salaryRange: '65,000-75,000',
-            highlights: [
-                'Create and/or enhance action-driven dashboards (e.g., using Tableau)',
-                'Perform ongoing checks of internal dashboards to ensure they are up to date.',
-                'Support ad hoc data, SQL query, analysis, and debugging requests from cross-functional stakeholders.'
-            ]
-        }
-    ];
+  const initialJobs = [
+    {
+      id: 1,
+      title: 'Jr. Software Development Engineer',
+      company: 'Amazon',
+      location: 'Seattle, WA',
+      industry: 'Technology',
+      salaryRange: '60,000-70,000',
+      highlights: [
+        'Part-time work during the school year (16 hours/week)',
+        'Full-time work during the summer (40 hours/week)',
+        'Effective performance management and integrated opportunities for growth'
+      ],
+      imageUrl: './WechatIMG623.jpg'
+    },
+    {
+      id: 2,
+      title: 'Data Analytics Intern',
+      company: 'DHL Express',
+      location: 'Dallas, TX',
+      industry: 'Logistics',
+      salaryRange: '30,000-40,000',
+      highlights: [
+        'Strong communication skills (written and verbal)',
+        'Strong attention to detail and organizational skills',
+        'Knowledge of business intelligence tools such as Power BI, Tableau'
+      ],
+      imageUrl: dataImg
+    },
+    {
+      id: 3,
+      title: 'Tax Data Analyst',
+      company: 'Salesforce',
+      location: 'Seattle, WA',
+      industry: 'Technology',
+      salaryRange: '70,000-80,000',
+      highlights: [
+        'Experience telling stories with data and familiarity with Tableau visualization tool',
+        'Experience applying your analytics skills to projects which have had impact on business strategy',
+        'Interest in unlocking new opportunities for growth by discovering insights, automating processes'
+      ],
+      imageUrl: UIUX
+    },
+    {
+      id: 4,
+      title: 'Data Analyst, Supply Chain',
+      company: 'Tesla',
+      location: 'Fremont, CA',
+      industry: 'Automotive',
+      salaryRange: '65,000-75,000',
+      highlights: [
+        'Create and/or enhance action-driven dashboards (e.g., using Tableau)',
+        'Perform ongoing checks of internal dashboards to ensure they are up to date.',
+        'Support ad hoc data, SQL query, analysis, and debugging requests from cross-functional stakeholders.'
+      ],
+      imageUrl: software
+    }
+  ];
 
-    const [jobs, setJobs] = useState(initialJobs);
-    const [searchParams, setSearchParams] = useState({
-        jobTitle: '',
-        jobLocation: '',
-        jobIndustry: '',
-        jobSalary: ''
+  const [jobs, setJobs] = useState(initialJobs);
+  const [searchParams, setSearchParams] = useState({
+    jobTitle: '',
+    jobLocation: '',
+    jobIndustry: '',
+    jobSalary: ''
+  });
+
+  const handleInputChange = (event) => {
+    setSearchParams({
+      ...searchParams,
+      [event.target.name]: event.target.value.toLowerCase()
     });
+  };
 
-    const handleInputChange = (event) => {
-        setSearchParams({
-            ...searchParams,
-            [event.target.name]: event.target.value.toLowerCase()
-        });
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    const filteredJobs = initialJobs.filter(job => (
+      job.title.toLowerCase().includes(searchParams.jobTitle) &&
+      job.location.toLowerCase().includes(searchParams.jobLocation) &&
+      job.industry.toLowerCase().includes(searchParams.jobIndustry) &&
+      job.salaryRange.toLowerCase().includes(searchParams.jobSalary)
+    ));
 
-        const filteredJobs = initialJobs.filter(job => (
-            job.title.toLowerCase().includes(searchParams.jobTitle) &&
-            job.location.toLowerCase().includes(searchParams.jobLocation) &&
-            job.industry.toLowerCase().includes(searchParams.jobIndustry) &&
-            job.salaryRange.toLowerCase().includes(searchParams.jobSalary)
-        ));
+    setJobs(filteredJobs);
+  };
 
-        setJobs(filteredJobs);
-    };
+  const renderJobListings = () => {
+    return jobs.map(job => (
+      <Col md={6} lg={4} key={job.id} className="mb-4">
+        <Card>
+          <Card.Body>
+            <Card.Title>{job.title}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">{job.company}</Card.Subtitle>
+            <Card.Text>
+              <strong>Location:</strong> {job.location}<br/>
+              <strong>Industry:</strong> {job.industry}<br/>
+              <strong>Salary Range:</strong> {job.salaryRange}
+            </Card.Text>
+            <Card.Text>
+              <strong>Role Highlights:</strong>
+              <ul>
+                {job.highlights.map((highlight, index) => (
+                  <li key={index}>{highlight}</li>
+                ))}
+              </ul>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    ));
+  };
 
-    const renderJobListings = () => {
-        return jobs.map(job => (
-            <div key={job.id} className="job-listing">
-                <h2>{job.title}</h2>
-                <h3>Company:</h3>
-                <h4>{job.company}</h4>
-                <h3>Location:</h3>
-                <h4>{job.location}</h4>
-                <h3>Industry:</h3>
-                <h4>{job.industry}</h4>
-                <h3>Salary Range:</h3>
-                <h4>{job.salaryRange}</h4>
-                <h3>Role Highlights:</h3>
-                <ul>
-                    {job.highlights.map((highlight, index) => (
-                        <li key={index}>{highlight}</li>
-                    ))}
-                </ul>
-            </div>
-        ));
-    };
+  return (
+    <main>
+      <Container>
+        <Carousel className="mb-4">
+          {initialJobs.map(job => (
+            <Carousel.Item key={job.id}>
+              <img
+                className="d-block w-100"
+                src={job.imageUrl}
+                alt={`Slide of ${job.title}`}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
 
-    return (
-        <main>
-            <div>
-                <img className={'listing-img'} src={'./WechatIMG623.jpg'} alt="Searching Photo"/>
-            </div>
-            <section>
-                <form onSubmit={handleSubmit} id="search-form">
-                    <div className={'text-center'}>
-                        <label htmlFor="jobTitleInput">Search by Job Title:</label>
-                        <input
-                            type="text"
-                            name="jobTitle"
-                            id="jobTitleInput"
-                            onChange={handleInputChange}
-                            value={searchParams.jobTitle}
-                            placeholder="Enter job title"
-                        />
-                        <label htmlFor="locationInput">Search by Location:</label>
-                        <input
-                            type="text"
-                            name="jobLocation"
-                            id="locationInput"
-                            onChange={handleInputChange}
-                            value={searchParams.jobLocation}
-                            placeholder="Enter location"
-                        />
-                        <label htmlFor="industryInput">Search by Industry:</label>
-                        <input
-                            type="text"
-                            name="jobIndustry"
-                            id="industryInput"
-                            onChange={handleInputChange}
-                            value={searchParams.jobIndustry}
-                            placeholder="Enter industry"
-                        />
-                        <label htmlFor="salaryInput">Search by Salary Range:</label>
-                        <input
-                            type="text"
-                            name="jobSalary"
-                            id="salaryExpeInput"
-                            onChange={handleInputChange}
-                            value={searchParams.jobSalary}
-                            placeholder="Enter salary range"
-                        />
-                    </div>
-                    <div className={'text-center'}>
-                        <button type="submit" aria-label="Apply Filter">
-                            <i className="fas fa-filter"></i> Apply Filter
-                        </button>
-                    </div>
-                </form>
-            </section>
+        <Form onSubmit={handleSubmit} id="search-form">
+          <Row className="mb-3">
+            <Col>
+              <Form.Group controlId="jobTitleInput">
+                <Form.Label>Search by Job Title:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="jobTitle"
+                  onChange={handleInputChange}
+                  value={searchParams.jobTitle}
+                  placeholder="Enter job title"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="locationInput">
+                <Form.Label>Search by Location:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="jobLocation"
+                  onChange={handleInputChange}
+                  value={searchParams.jobLocation}
+                  placeholder="Enter location"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="industryInput">
+                <Form.Label>Search by Industry:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="jobIndustry"
+                  onChange={handleInputChange}
+                  value={searchParams.jobIndustry}
+                  placeholder="Enter industry"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="salaryInput">
+                <Form.Label>Search by Salary Range:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="jobSalary"
+                  onChange={handleInputChange}
+                  value={searchParams.jobSalary}
+                  placeholder="Enter salary range"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Button type="submit" className="mb-3">
+            Apply Filter
+          </Button>
+        </Form>
 
-            {renderJobListings()}
-        </main>
-    );
+        <Row>
+          {renderJobListings()}
+        </Row>
+      </Container>
+    </main>
+  );
 };
 
 export default Listings;
+
+ 
